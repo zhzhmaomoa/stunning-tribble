f65bc31d79fb58a33e5866f99de186d12d578942
zhzhl <liuzhizhen@qluanyun.com>
2025-01-15 15:26:11 +0800

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -48,6 +48,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error)
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -79,6 +80,7 @@ export async function updateInvoice(id: string,prevState: State, formData: FormD
         WHERE id = ${id}
       `;
   } catch (error) {
+    console.error(error)
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
@@ -91,6 +93,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
+    console.error(error)
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
